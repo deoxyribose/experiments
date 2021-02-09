5eacec8169d57dec1555f9f149eba74823768f91
deoxyribose <frzdyb@gmail.com>
2021-02-09 18:17:22 +0100

    A bunch of stuff

--- model.py
+++ model.py
@@ -1,113 +1,179 @@
 from models_and_guides import *
 
-class DAGModel(Model):
+class DAGmodel(Model):
     """
     Template class for code generated from a DAG
     """
-    def __init__(self, X, K, C, batch_size, _id):
-        self.K = K
-        self.C = C
-        super(DAGModel, self).__init__(X, batch_size, _id)
 
+    def __init__(self, X, batch_size, _id):
+        super(DAGmodel, self).__init__(X, batch_size, _id)
 
     def get_param_shapes_and_support(self, _id=None):
         if _id == None:
             _id = self._id
-        return {f'mixing_proportions_concentration_init_{_id}': ((self.C,),
-            constraints.positive),
-            f'mixing_proportions_prior_concentration_init_{_id}': ((self.C,),
-            constraints.positive), f'cov_factor_scale_init_{_id}': ((self.K,
-            self.D), constraints.positive),
-            f'cov_factor_prior_scale_init_{_id}': ((self.K, self.D),
-            constraints.positive), f'cov_factor_loc_init_{_id}': ((self.C,self.K, self
-            .D), constraints.real), f'cov_factor_prior_loc_init_{_id}': ((self.C,self.
-            K, self.D), constraints.real), f'cov_diag_scale_init_{_id}': ((self
-            .D,), constraints.positive), f'cov_diag_prior_scale_init_{_id}': ((
-            self.D,), constraints.positive), f'cov_diag_loc_init_{_id}': ((self
-            .D,), constraints.real), f'cov_diag_prior_loc_init_{_id}': ((self.D
-            ,), constraints.real)}
+        return {f'X_scale_init_{_id}': ((self.D,), constraints.positive),
+            f'X_prior_scale_init_{_id}': ((self.D,), constraints.positive),
+            f'X_loc_init_{_id}': ((self.D,), constraints.real),
+            f'X_prior_loc_init_{_id}': ((self.D,), constraints.real)}
 
+    def model(self, X):
+        N, D = X.shape
+        _id = self._id
+        X_prior_scale_init = self.param_init[f'X_prior_scale_init_{_id}']
+        X_prior_scale = pyro.param(f'X_prior_scale_{_id}',
+            X_prior_scale_init, constraint=constraints.positive)
+        X_prior_loc_init = self.param_init[f'X_prior_loc_init_{_id}']
+        X_prior_loc = pyro.param(f'X_prior_loc_{_id}', X_prior_loc_init)
+        with pyro.plate(f'N_{_id}', N, subsample_size=self.batch_size) as ind:
+            X = pyro.sample(f'X_{_id}', dist.Normal(X_prior_loc,
+                X_prior_scale).to_event(1))
+        return X
+
+    def guide(self, X):
+        N, D = X.shape
+        _id = self._id
+        return ()
+from models_and_guides import *
+
+class DAGmodel(Model):
+    """
+    Template class for code generated from a DAG
+    """
+
+    def __init__(self, X, batch_size, _id):
+        super(DAGmodel, self).__init__(X, batch_size, _id)
+
+    def get_param_shapes_and_support(self, _id=None):
+        if _id == None:
+            _id = self._id
+        return {f'X_scale_init_{_id}': ((self.D,), constraints.positive),
+            f'X_prior_scale_init_{_id}': ((self.D,), constraints.positive),
+            f'X_loc_init_{_id}': ((self.D,), constraints.real),
+            f'X_prior_loc_init_{_id}': ((self.D,), constraints.real)}
+
+    def model(self, X):
+        N, D = X.shape
+        _id = self._id
+        X_prior_scale_init = self.param_init[f'X_prior_scale_init_{_id}']
+        X_prior_scale = pyro.param(f'X_prior_scale_{_id}',
+            X_prior_scale_init, constraint=constraints.positive)
+        X_prior_loc_init = self.param_init[f'X_prior_loc_init_{_id}']
+        X_prior_loc = pyro.param(f'X_prior_loc_{_id}', X_prior_loc_init)
+        with pyro.plate(f'N_{_id}', N, subsample_size=self.batch_size) as ind:
+            X = pyro.sample('obs', dist.Normal(X_prior_loc, X_prior_scale).
+                to_event(1), obs=X.index_select(0, ind))
+        return X
+
+    def guide(self, X):
+        N, D = X.shape
+        _id = self._id
+        return ()
+from models_and_guides import *
+
+class DAGmodel(Model):
+    """
+    Template class for code generated from a DAG
+    """
+
+    def __init__(self, X, K, batch_size, _id):
+        self.K = K
+        super(DAGmodel, self).__init__(X, batch_size, _id)
+
+    def get_param_shapes_and_support(self, _id=None):
+        if _id == None:
+            _id = self._id
+        return {f'X_scale_scale_init_{_id}': ((self.D,), constraints.
+            positive), f'X_scale_prior_scale_init_{_id}': ((self.D,),
+            constraints.positive), f'X_scale_loc_init_{_id}': ((self.D,),
+            constraints.real), f'X_scale_prior_loc_init_{_id}': ((self.D,),
+            constraints.real), f'W_scale_init_{_id}': ((self.D, self.K),
+            constraints.positive), f'W_prior_scale_init_{_id}': ((self.D,
+            self.K), constraints.positive), f'W_loc_init_{_id}': ((self.D,
+            self.K), constraints.real), f'W_prior_loc_init_{_id}': ((self.D,
+            self.K), constraints.real), f'z_scale_init_{_id}': ((self.K,),
+            constraints.positive), f'z_prior_scale_init_{_id}': ((self.K,),
+            constraints.positive), f'z_loc_init_{_id}': ((self.K,),
+            constraints.real), f'z_prior_loc_init_{_id}': ((self.K,),
+            constraints.real), f'loc_scale_init_{_id}': ((self.D,),
+            constraints.positive), f'loc_prior_scale_init_{_id}': ((self.D,
+            ), constraints.positive), f'loc_loc_init_{_id}': ((self.D,),
+            constraints.real), f'loc_prior_loc_init_{_id}': ((self.D,),
+            constraints.real)}
 
     def model(self, X):
         K = self.K
-        C = self.C
         N, D = X.shape
         _id = self._id
-        cov_diag_prior_loc_init = self.param_init[f'cov_diag_prior_loc_init_{_id}']
-        cov_diag_prior_loc = pyro.param(f'cov_diag_prior_loc_{_id}',
-            cov_diag_prior_loc_init)
-        cov_diag_prior_scale_init = self.param_init[
-            f'cov_diag_prior_scale_init_{_id}']
-        cov_diag_prior_scale = pyro.param(f'cov_diag_prior_scale_{_id}',
-            cov_diag_prior_scale_init, constraint=constraints.positive)
-        cov_factor_prior_loc_init = self.param_init[
-            f'cov_factor_prior_loc_init_{_id}']
-        cov_factor_prior_loc = pyro.param(f'cov_factor_prior_loc_{_id}',
-            cov_factor_prior_loc_init)
-        cov_factor_prior_scale_init = self.param_init[
-            f'cov_factor_prior_scale_init_{_id}']
-        cov_factor_prior_scale = pyro.param(f'cov_factor_prior_scale_{_id}',
-            cov_factor_prior_scale_init, constraint=constraints.positive)
-        prior_loc = torch.zeros(D)
-        mixing_proportions_prior_concentration_init = self.param_init[
-            f'mixing_proportions_prior_concentration_init_{_id}']
-        mixing_proportions_prior_concentration = pyro.param(
-            f'mixing_proportions_prior_concentration_{_id}',
-            mixing_proportions_prior_concentration_init, constraint=constraints
-            .positive)
-        mixing_proportions = pyro.sample(f'mixing_proportions_{_id}', dist.
-            Dirichlet(mixing_proportions_prior_concentration))
-        with pyro.plate(f'C_{_id}', C):
-            with pyro.plate(f'CD_{_id}', D):
-                with pyro.plate(f'CDK_{_id}', K):
-                    cov_factor = pyro.sample(f'cov_factor_{_id}', dist.Normal(
-                        cov_factor_prior_loc, cov_factor_prior_scale))
-                cov_diag = pyro.sample(f'cov_diag_{_id}', dist.LogNormal(
-                    cov_diag_prior_loc, cov_diag_prior_scale))
-            cov_factor_T = torch.transpose(cov_factor, 0, 1)
+        X_scale_prior_scale_init = self.param_init[
+            f'X_scale_prior_scale_init_{_id}']
+        X_scale_prior_scale = pyro.param(f'X_scale_prior_scale_{_id}',
+            X_scale_prior_scale_init, constraint=constraints.positive)
+        X_scale_prior_loc_init = self.param_init[
+            f'X_scale_prior_loc_init_{_id}']
+        X_scale_prior_loc = pyro.param(f'X_scale_prior_loc_{_id}',
+            X_scale_prior_loc_init)
+        X_scale = pyro.sample(f'X_scale_{_id}', dist.LogNormal(
+            X_scale_prior_loc, X_scale_prior_scale).to_event(1))
+        X_scale_jitter = torch.add(X_scale, 0.0001)
+        W_prior_scale_init = self.param_init[f'W_prior_scale_init_{_id}']
+        W_prior_scale = pyro.param(f'W_prior_scale_{_id}',
+            W_prior_scale_init, constraint=constraints.positive)
+        W_prior_loc_init = self.param_init[f'W_prior_loc_init_{_id}']
+        W_prior_loc = pyro.param(f'W_prior_loc_{_id}', W_prior_loc_init)
+        W = pyro.sample(f'W_{_id}', dist.Normal(W_prior_loc, W_prior_scale)
+            .to_event(2))
+        z_prior_scale_init = self.param_init[f'z_prior_scale_init_{_id}']
+        z_prior_scale = pyro.param(f'z_prior_scale_{_id}',
+            z_prior_scale_init, constraint=constraints.positive)
+        z_prior_loc_init = self.param_init[f'z_prior_loc_init_{_id}']
+        z_prior_loc = pyro.param(f'z_prior_loc_{_id}', z_prior_loc_init)
+        X_loc = torch.add(Wz, loc)
         with pyro.plate(f'N_{_id}', N, subsample_size=self.batch_size) as ind:
-            assignment = pyro.sample(f'assignment_{_id}', dist.Categorical(
-                mixing_proportions))
-            loc_idx = torch.index_select(prior_loc, assignment, 0)
-            cov_factor_T_idx = torch.index_select(cov_factor_T, assignment, 0)
-            cov_diag_idx = torch.index_select(cov_diag, assignment, 0)
-            X = pyro.sample('obs', dist.LowRankMultivariateNormal(loc_idx,
-                cov_factor_T_idx, cov_diag_idx), obs=X.index_select(0, ind))
+            z = pyro.sample(f'z_{_id}', dist.Normal(z_prior_loc,
+                z_prior_scale).to_event(1))
+            X = pyro.sample('obs', dist.Normal(X_loc, X_scale_jitter).
+                to_event(1), obs=X.index_select(0, ind))
+        Wz = torch.matmul(z, W.T)
+        loc_prior_scale_init = self.param_init[f'loc_prior_scale_init_{_id}']
+        loc_prior_scale = pyro.param(f'loc_prior_scale_{_id}',
+            loc_prior_scale_init, constraint=constraints.positive)
+        loc_prior_loc_init = self.param_init[f'loc_prior_loc_init_{_id}']
+        loc_prior_loc = pyro.param(f'loc_prior_loc_{_id}', loc_prior_loc_init)
+        loc = pyro.sample(f'loc_{_id}', dist.Normal(loc_prior_loc,
+            loc_prior_scale).to_event(1))
         return X
-
 
     def guide(self, X):
         K = self.K
-        C = self.C
         N, D = X.shape
         _id = self._id
-        cov_diag_loc_init = self.param_init[f'cov_diag_loc_init_{_id}']
-        cov_diag_loc = pyro.param(f'cov_diag_loc_{_id}', cov_diag_loc_init)
-        cov_diag_scale_init = self.param_init[f'cov_diag_scale_init_{_id}']
-        cov_diag_scale = pyro.param(f'cov_diag_scale_{_id}',
-            cov_diag_scale_init, constraint=constraints.positive)
-        cov_factor_loc_init = self.param_init[f'cov_factor_loc_init_{_id}']
-        cov_factor_loc = pyro.param(f'cov_factor_loc_{_id}', cov_factor_loc_init)
-        cov_factor_scale_init = self.param_init[f'cov_factor_scale_init_{_id}']
-        cov_factor_scale = pyro.param(f'cov_factor_scale_{_id}',
-            cov_factor_scale_init, constraint=constraints.positive)
-        loc = torch.zeros(D)
-        mixing_proportions_concentration_init = self.param_init[
-            f'mixing_proportions_concentration_init_{_id}']
-        mixing_proportions_concentration = pyro.param(
-            f'mixing_proportions_concentration_{_id}',
-            mixing_proportions_concentration_init, constraint=constraints.positive)
-        mixing_proportions = pyro.sample(f'mixing_proportions_{_id}', dist.
-            Dirichlet(mixing_proportions_concentration))
+        X_scale_scale_init = self.param_init[f'X_scale_scale_init_{_id}']
+        X_scale_scale = pyro.param(f'X_scale_scale_{_id}',
+            X_scale_scale_init, constraint=constraints.positive)
+        X_scale_loc_init = self.param_init[f'X_scale_loc_init_{_id}']
+        X_scale_loc = pyro.param(f'X_scale_loc_{_id}', X_scale_loc_init)
+        X_scale = pyro.sample(f'X_scale_{_id}', dist.LogNormal(X_scale_loc,
+            X_scale_scale).to_event(1))
+        W_scale_init = self.param_init[f'W_scale_init_{_id}']
+        W_scale = pyro.param(f'W_scale_{_id}', W_scale_init, constraint=
+            constraints.positive)
+        W_loc_init = self.param_init[f'W_loc_init_{_id}']
+        W_loc = pyro.param(f'W_loc_{_id}', W_loc_init)
+        W = pyro.sample(f'W_{_id}', dist.Normal(W_loc, W_scale).to_event(2))
+        z_scale_init = self.param_init[f'z_scale_init_{_id}']
+        z_scale = pyro.param(f'z_scale_{_id}', z_scale_init, constraint=
+            constraints.positive)
+        z_loc_init = self.param_init[f'z_loc_init_{_id}']
+        z_loc = pyro.param(f'z_loc_{_id}', z_loc_init)
         with pyro.plate(f'N_{_id}', N, subsample_size=self.batch_size) as ind:
-            assignment = pyro.sample(f'assignment_{_id}', dist.Categorical(
-                mixing_proportions))
-        with pyro.plate(f'C_{_id}', C):
-            with pyro.plate(f'CD_{_id}', D):
-                with pyro.plate(f'CDK_{_id}', K):
-                    cov_factor = pyro.sample(f'cov_factor_{_id}', dist.Normal(
-                        cov_factor_loc, cov_factor_scale))
-                cov_diag = pyro.sample(f'cov_diag_{_id}', dist.LogNormal(
-                    cov_diag_loc, cov_diag_scale))
-            cov_factor_T = torch.transpose(cov_factor, 0, 1)
-        return cov_factor, cov_diag, mixing_proportions, assignment
+            z = pyro.sample(f'z_{_id}', dist.Normal(z_loc, z_scale).to_event(1)
+                )
+        Wz = torch.matmul(z, W.T)
+        loc_scale_init = self.param_init[f'loc_scale_init_{_id}']
+        loc_scale = pyro.param(f'loc_scale_{_id}', loc_scale_init,
+            constraint=constraints.positive)
+        loc_loc_init = self.param_init[f'loc_loc_init_{_id}']
+        loc_loc = pyro.param(f'loc_loc_{_id}', loc_loc_init)
+        loc = pyro.sample(f'loc_{_id}', dist.Normal(loc_loc, loc_scale).
+            to_event(1))
+        return X_scale, z, W, loc
